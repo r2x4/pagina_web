6152fcd98170f8e6782a4b0f2ff26b210b775958
Rodrigo <rrs230780@gmail.com>
2025-02-27 20:37:41 -0500

    guardar

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Contador from "./componentes/contador";
 function App() {
   return (
     <div>
-      <h1>¡Mi primer contador con React y Vite! 🚀</h1>
+      <h1>¡Rodrigo Mi primer contador con React y Vite! 🚀</h1>
       <Contador />
     </div>
   );
